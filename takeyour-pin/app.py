5b5f0e31958661e0b01897f279bf33554f2e09da
rodrigmars <rodrigmars@gmail.com>
2023-03-02 17:47:15 -0300

    Ajustes no retorno da rota pin

--- takeyour-pin/app.py
+++ takeyour-pin/app.py
@@ -1,17 +1,38 @@
-from bottle import route, run
+
+from json import dumps
+from bottle import route, run, HTTPResponse
 from core.user import user
 from core.pin import pin
 
+
+def http_response(status_code: int, data: str) -> HTTPResponse:
+    return HTTPResponse(status=status_code, body=dumps(data))
+
+
+def response_builder(code: int, data: dict) -> dict:
+    return {"status_code": code,
+            "data": data}
+
+
 @route('/pin/<email>')
-def index(email: str) -> dict:
-    
+def index(email: str):
+
+    response: dict = {}
     code: str | None = None
 
-    if user()(email):
+    try:
+        if user()(email):
 
-        code = pin()()
+            code = pin()()
 
-    return {'pin': code}
+        response.update(response_builder(200, {'pin': code}))
+
+    except Exception:
+        response.update(response_builder(500, {'error': "Unknown error"}))
+    finally:
+        return http_response(**response)
 
 
-run(debug=True, reloader=True, host='localhost', port=8080)
+if __name__ == "__main__":
+
+    run(debug=True, reloader=True, host='localhost', port=8080)
